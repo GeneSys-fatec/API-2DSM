142ee4215b07e05b371896b56f5c878e592d5f3a
Ana Gaspar <anajulia3907@gmail.com>
2025-03-17 19:30:29 -0300

    GEN-5 Modificado os componentes principais

--- app/frontend/src/components/layout/Cards.tsx
+++ app/frontend/src/components/layout/Cards.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+interface CardsProps {
+    tituloCard: string,
+    iconeCard: string,
+    valorCard: number
+}
 
-const Cards = () => {
+const Cards: React.FC<CardsProps> = ({ tituloCard, iconeCard, valorCard }) => {
     return (
-        <div></div>
+        <div>
+            <img src={ iconeCard }/>
+            <h3>{ valorCard }</h3>
+            <p>{ tituloCard }</p>
+        </div>
     )
 }
 
