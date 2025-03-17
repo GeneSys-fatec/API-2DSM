ad3a0a1e774e7f894126366879ea4d0ff6d8dcc8
Matheus-Roberto-Castro <matheusrobertocastros@outlook.com>
2025-03-17 19:51:40 -0300

    GEN-11: Merge da branch GEN-5

--- app/frontend/src/components/layout/Cards.tsx
+++ app/frontend/src/components/layout/Cards.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
-import "./Cards.scss"
-=======
 interface CardsProps {
     tituloCard: string,
     iconeCard: string,
     valorCard: number
 }
->>>>>>> 26329ad1df3102368b38ccbd1caa98ce535cc17f
 
 const Cards: React.FC<CardsProps> = ({ tituloCard, iconeCard, valorCard }) => {
     return (
