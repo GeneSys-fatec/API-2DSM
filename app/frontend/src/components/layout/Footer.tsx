cdc939c5963f1918cfe5a5bf224bdf84217e16f7
Matheus-Roberto-Castro <matheusrobertocastros@outlook.com>
2025-03-17 19:45:11 -0300

    GEN-11: Criado arquivos scss

--- app/frontend/src/components/layout/Footer.tsx
+++ app/frontend/src/components/layout/Footer.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import "./Footer.scss"
 
 const Footer = () => {
     return (
-        <div>
-            <img src="https://img.icons8.com/?size=100&id=Xy10Jcu1L2Su&format=png&color=000000" alt="Instagram" />
-            <img src="https://img.icons8.com/?size=100&id=16713&format=png&color=000000" alt="WhatsApp" />
-            <img src="https://img.icons8.com/?size=100&id=QiwSMfboPt2R&format=png&color=000000" alt="Site" />
-            <h1>Sobre</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero repellendus quo, maiores incidunt debitis recusandae id rerum, totam dolorem facilis aliquid fugiat, temporibus maxime consequuntur repudiandae unde et sit voluptas.</p>
+        <div className="footer">
+            <div className="contatos">
+                <img src="https://img.icons8.com/?size=100&id=Xy10Jcu1L2Su&format=png&color=000000"     alt="Instagram" />
+                <img src="https://img.icons8.com/?size=100&id=16713&format=png&color=000000" alt="WhatsApp" />
+                <img src="https://img.icons8.com/?size=100&id=QiwSMfboPt2R&format=png&color=000000" alt="Site" />
+            </div>
+            <div className="texto">
+                <h1>Sobre</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero repellendus quo, maiores incidunt debitis recusandae id rerum, totam dolorem facilis aliquid fugiat, temporibus maxime consequuntur repudiandae unde et sit voluptas.</p>
+            </div>
         </div>
     )
 }
