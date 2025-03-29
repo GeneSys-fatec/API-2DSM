9e1b440e378bb1d801f64f89d1c0f22b0a914f0f
Matheus-Roberto-Castro <matheusrobertocastros@outlook.com>
2025-03-29 11:42:48 -0300

    feat[front](GEN-22): Adicionado gráfico com dados fictícios

--- app/frontend/src/components/layout/UsuariosInseridos.tsx
+++ app/frontend/src/components/layout/UsuariosInseridos.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "./UsuariosInseridos.scss";
+import { BarChart, Bar, Rectangle, ResponsiveContainer, XAxis, YAxis, CartesianGrid } from "recharts";
 
 interface UsuariosImpactadosProps {
   totalDoisAnos: number;
   totalSeisMeses: number;
 }
+
+const info = [
+  { name: "Jan", quantidade: 1234 },
+  { name: "Fev", quantidade: 1321 },
+  { name: "Mar", quantidade: 1432 },
+  { name: "Abr", quantidade: 1132 },
+  { name: "Mai", quantidade: 1390 },
+  { name: "Jun", quantidade: 1234 },
+  { name: "Jul", quantidade: 1321 },
+  { name: "Ago", quantidade: 1432 },
+  { name: "Set", quantidade: 1132 },
+  { name: "Out", quantidade: 1390 }
+];
 
 const UsuariosImpactados: React.FC<UsuariosImpactadosProps> = ({ totalDoisAnos, totalSeisMeses }) => {
   return (
@@ -12,6 +26,19 @@ const UsuariosImpactados: React.FC<UsuariosImpactadosProps> = ({ totalDoisAnos, 
       <h3>Usuários Impactados</h3>
       <p>Total últimos 2 anos: {totalDoisAnos}</p>
       <p>Total últimos 6 meses: {totalSeisMeses}</p>
+            <div>
+            <ResponsiveContainer width={700} height={400}>
+              <BarChart
+                width={500}
+                height={400}
+                data={info}>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis dataKey="name" fontSize={20}/>
+                <YAxis fontSize={20}/>
+                <Bar dataKey="quantidade" fill="#69d6ce" barSize={30} activeBar={<Rectangle fill="#143357"/>}/>
+              </BarChart>
+            </ResponsiveContainer>
+            </div>
     </div>
   );
 };
