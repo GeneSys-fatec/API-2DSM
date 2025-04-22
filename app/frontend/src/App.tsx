77192d63bd7c532e43e0448ba284f8a16170cb02
Matheus-Roberto-Castro <matheusrobertocastros@outlook.com>
2025-04-22 16:14:03 -0300

    feat[front](GEN-33):Dividida a Aplicação em Rotas

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,40 +1,18 @@
-import './App.scss';
-import Header from "./components/pages/Dashboard/Header";
-import Footer from "./components/pages/Dashboard/Footer";
-import LojasCriadas from "./components/pages/Dashboard/LojasCriadas"
-import Patrocinado from "./components/pages/Dashboard/Patrocinado"
-import Navbar from "./components/layout/Navbar"
-import UsuariosInseridos from "./components/pages/Dashboard/UsuariosInseridos"
-import "./components/pages/Dashboard/Cards.scss"
-import Mapa from './components/pages/Dashboard/Mapa';
-import Cadastro from './components/pages/Registro/Cadastro'
-import Login from './components/pages/Registro/Login'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/pages/Dashboard';
+import Registro from './components/pages/Registro';
+import Login from './components/pages/Login';
+ 
 function App() {
   return (
-    <body>
-      <Navbar linkInstagram="https://www.instagram.com/agenciaship/" linkWpp="https://api.whatsapp.com/send?phone=5512991235705" linkSite="https://agenciaship.com.br/" ></Navbar>
-      <div>
-        <Header logoEmpresa="https://agenciaship.com.br/wp-content/uploads/2023/05/Design-sem-nome-6-1-300x300.png" nomeEmpresa="Agência Ship" />
-        <div className='dashboard'>
-          <div className='cards-esquerda'>
-            <Patrocinado valorCard={322} />
-            <LojasCriadas valorCard={46} />
-            <LojasCriadas valorCard={46} />
-            {/* <Comunidades valorCard={10} /> Novo card */}
-          </div>
-          <div className='usuarios-impactados'>
-            <UsuariosInseridos ultimos3Dias={50} ultimaSemana={127} />
-          </div>
-          <div className='distribuicao-geografica'>
-            <Mapa />
-          </div>
-        </div>
-        <Footer descEmpresa="Agência SHIP traça estratégias práticas para anunciar seu negócio online e encontrar clientes nas principais plataformas disponíveis para o Marketing Digital."/>
-      </div>
-    </body>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/Registro" element={<Registro />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
-
-
+ 
 export default App;
--- app/frontend/src/components/pages/Dashboard_Componentes/Mapa.tsx
+++ app/frontend/src/components/pages/Dashboard_Componentes/Mapa.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import type { LatLngTuple, LatLngBoundsExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import '../Dashboard/Mapa.scss';
+import './Mapa.scss';
 import L from 'leaflet';
 
 L.Icon.Default.mergeOptions({
