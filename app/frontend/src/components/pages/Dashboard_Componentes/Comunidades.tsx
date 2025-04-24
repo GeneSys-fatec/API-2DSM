98e6f097cc0bb196a0bfa46db54afb339924de44
Lavínia Piratello <145591879+laviniappiratello@users.noreply.github.com>
2025-04-23 22:45:34 -0300

    feat[front](GEN-16): criação do card do mapa e props de comunidade

---
+++ app/frontend/src/components/pages/Dashboard_Componentes/Comunidades.tsx
@@ -0,0 +1,24 @@
+import "./Comunidades.scss";
+import React from "react";
+
+interface ComunidadesProps {
+  valorCard: number;
+}
+
+const Comunidades: React.FC<ComunidadesProps> = ({ valorCard }) => {
+  return (
+    <div className="card">
+      <div className="conteudo">
+        <div className="conteudo-card">
+          <img src="https://img.icons8.com/?size=100&id=122636&format=png&color=143357" />
+          <div className="dados-card">
+            <h3>{valorCard}</h3>
+            <p>Comunidades</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Comunidades;
